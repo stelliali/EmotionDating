4813c474b3cbb28b80ca35c06015bb52cf44fd4b
unknown <sofie.henghuber@campus.lmu.de>
2022-12-06 20:41:39 +0100

    anpassungen

--- Backend/main-image.py
+++ Backend/main-image.py
@@ -5,12 +5,19 @@ import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 from os.path import exists
+import shutil
 
 currentframe = 0
 
+shutil.copy('Downloads/statement1.webm', 'input/statement1.webm')
+shutil.copy('Downloads/statement2.webm', 'input/statement2.webm')
+shutil.copy('Downloads/statement3.webm', 'input/statement3.webm')
+shutil.copy('Downloads/statement4.webm', 'input/statement4.webm')
+shutil.copy('Downloads/statement5.webm', 'input/statement5.webm')
+
 while(True):
-    if exists("tests/test.mp4"):
-        cam = cv2.VideoCapture("tests/test.mp4")
+    if exists("input/statement5.webm"):
+        cam = cv2.VideoCapture("input/statement5.webm")
         break
 		
 while(True):
@@ -57,6 +64,6 @@ df = pd.read_csv("emotionvalues.csv")
 
 fig = df.plot(figsize=(20, 16), fontsize=26).get_figure()
 #fig = df.plot()
-fig.savefig('result-fe/my_figure.png')
+fig.savefig('result-fe/my_figureLive.png')
 
 plt.show()
--- Backend/src/fer/converter.py
+++ Backend/src/fer/converter.py
@@ -1,15 +1,16 @@
 import cv2
 
-cap = cv2.VideoCapture('statement5.webm')
+def convert(x):
+    cap = cv2.VideoCapture('input\statement'+x+'.webm')
 
-i=0
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    if i%3 == 0:
-        cv2.imwrite('C:\Users\SofieLouise\IdeaProjects\EmotionDating\Backend\output\tester'+str(i)+'.jpg',frame)
-    i+=1
+    i=0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        if i%3 == 0:
+            cv2.imwrite('input\demo'+str(i)+'.jpg',frame)
+        i+=1
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
