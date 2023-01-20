016904fe8b5d841f713052850ffd2150c0638316
qinzi li <stella.li19971118@outlook.com>
2023-01-20 22:14:47 +0100

    0.686

--- Backend/cosSimilarity.py
+++ Backend/cosSimilarity.py
@@ -1,7 +1,39 @@
 from numpy import dot
 from numpy.linalg import norm
 
-List1 = [0.53,0.0,0.04,0.31,0.08,0.02,0.01]
-List2 = [0.09,0.0,0.07,0.12,0.59,0.0,0.12]
-result = dot(List1, List2)/(norm(List1)*norm(List2))
-print(result)
+import csv
+
+l = []
+totalAve = [0.882167609706316, 0.7004884563419405, 0.5309750422771653, 0.641727334981321, 0.7834747699543595, 0.4750877451627905, 0.6342156695914987, 0.819629974402912, 0.5998219025088113, 0.7944509942098629]
+def cos(i):
+    with open('D:/ProgramData/AC/EmotionDating/Backend/user_study_vecs.csv', 'r') as vec:
+        reader = list(csv.reader(vec))
+        for index, rows in enumerate(reader):
+            if index == i:
+                v1 = rows
+            if index == i+17:
+                v2 = rows
+                
+
+    a = [float(x) for x in v1]   
+    b = [float(x) for x in v2]         
+    cos_sim = dot(a, b)/(norm(a)*norm(b))
+    # l.append(cos_sim)
+    return cos_sim
+
+    # with open('D:/ProgramData/AC/EmotionDating/Backend/user_study_re.csv',  'w', encoding='UTF8') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerow(l)
+
+if __name__ == "__main__":
+    # n = 9*35
+    # for i in range(n+3, n+18):
+    #     a = cos(i)
+    #     l.append(a)
+    #     ave = sum(l)/len(l)
+    #     print(l)
+    #     print(ave)
+        
+    totalave = sum(totalAve)/len(totalAve)
+    print(totalave)
+    # 0.6862039499136978
